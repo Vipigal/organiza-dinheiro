93f1a029e4a905c8b40a337c4c5f35334cb14984
Vinicius Pinho <viniciuspgalvao@hotmail.com>
2024-05-14 01:41:31 -0300

    fix: remove userButton from dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,11 @@
 import Container from "@/components/Container";
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <Container>
-      <UserButton afterSignOutUrl="/" showName />;
+      <span>
+        <h1 className="text-4xl font-bold ml-28 mt-20">Dashboard</h1>
+      </span>
     </Container>
   );
 }
